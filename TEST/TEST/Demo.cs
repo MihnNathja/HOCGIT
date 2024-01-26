d443c26ff2a7d22cecf70c8b466c0ed11e180cb5
ChimSe <22110377@student.hcmute.edu.vn>
2024-01-26 16:02:38 +0700

    Update Demo

--- TEST/TEST/Demo.cs
+++ TEST/TEST/Demo.cs
@@ -10,10 +10,14 @@ namespace TEST
     {
         int x;
         int y;
-        int z = x + y * x + y;
-        int y = 5;
+
+        int z = X + Y * X + Y;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
 
         int z = 2;
         int j = 5;
+
     }
 }
